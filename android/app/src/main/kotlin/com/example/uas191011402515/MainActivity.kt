d883f2fbf91c0ecf1525dcf757fd5373fc04037a
NovitaFP <novitafp50@gmail.com>
2022-04-09 16:45:24 +0700

    projek kedua

---
+++ android/app/src/main/kotlin/com/example/uas191011402515/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uas191011402515
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/second_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart'
+    show
+        AppBar,
+        BuildContext,
+        Center,
+        Column,
+        FloatingActionButton,
+        Icon,
+        Icons,
+        Key,
+        Scaffold,
+        StatelessWidget,
+        Text,
+        Widget;
+import 'package:provider/provider.dart';
+import "package:uas191011402515/providers/counter_provider.dart";
+import 'package:uas191011402515/providers/shopping_cart_provider.dart';
+
+class SecondScreen extends StatelessWidget {
+  SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider Example App (${context.watch<Counter>().count})'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('${context.watch<ShoppingCart>().count}'),
+            Text('${context.watch<ShoppingCart>().cart}')
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: Key('addItem_floatingActionButton'),
+        onPressed: () => context.read<ShoppingCart>().addItem('Bread'),
+        tooltip: 'Add Item',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
